c2fcb478bf1d7b21a9968c13d4a5effe4d20e6e1
Ahmed Shaalan <ahmed_shaalan@Ahmeds-MacBook-Pro.local>
2016-12-29 23:49:57 +0200

    Initial commit

---
+++ Appender.sketchplugin/Contents/Sketch/append_text.js
@@ -0,0 +1,141 @@
+var nonTextSelected = 0
+
+function onRun(context) {
+
+  // "An NSApplication object manages an app’s main event loop in addition to resources used by all of that app’s objects." - Apple
+  var app = [NSApplication sharedApplication]
+
+  // get sketch context
+  var sketch = context.api()
+
+  // get document
+  var document = context.document
+
+  // get selection
+  var selection = context.selection
+
+  // number of layers selected
+  var numOfLayers = selection.count()
+
+  // check if there are no layers selected
+  if (!numOfLayers) {
+    [app displayDialog:"Select me some text layers, Please :/" withTitle:"Appender"]
+    return
+  }
+
+  // get user input
+  var userInput = getUserInput(numOfLayers)
+
+  // get pressed button
+  var userPressedButton = userInput[0] == 1000 ? "Append" : "Cancel"
+
+  // get user text input
+  var userTextInput = userInput[1]
+
+  // check if user pressed append
+  if (userPressedButton == "Append") {
+
+    // call append function
+    doTheThing (selection, numOfLayers, userTextInput)
+
+    // notify if there are any non text layer selected
+    if (nonTextSelected) {
+      document.hideMessage()  // hide previous message
+      document.showMessage("Non-text layer selected, nothing happened to them ^_^")
+    }
+
+    // save userInput to use it in appendAgain
+    sketch.setSettingForKey("userTextInput", userTextInput)
+  }
+
+}
+
+
+// append previous input to current selection
+function appendAgain(context) {
+
+  // "An NSApplication object manages an app’s main event loop in addition to resources used by all of that app’s objects." - Apple
+  var app = [NSApplication sharedApplication]
+
+  // get sketch context
+  var sketch = context.api()
+
+  // get previous user input
+  var previousUserInput = sketch.settingForKey("userTextInput")
+
+  // check if its not null
+  if (previousUserInput != null) {
+
+    // get document & selection
+    var document = context.document
+    var selection = context.selection
+
+    // number of layers selected
+    var numOfLayers = selection.count()
+
+    // append text
+    doTheThing (selection, numOfLayers, previousUserInput)
+
+  }else {
+    [app displayDialog:"I don't remember that you asked me to append text before :-|" withTitle:"Appender"]
+  }
+}
+
+
+
+// brain function
+function doTheThing (selection, numOfLayers, userTextInput)
+{
+  // check layer class, then append text if its a text layer
+  var item
+  for (item = 0; item < numOfLayers; item++) {
+
+    // get current layer & its class
+    var layer = selection[item]
+    var layerClass = layer.class()
+
+    // check layer class if Text layer then append text
+    if (layerClass == MSTextLayer) {
+      var newText =  layer.stringValue + userTextInput
+      layer.stringValue = layer.stringValue() + userTextInput
+
+    } else {
+      nonTextSelected = 1
+    }
+  }
+
+}
+
+
+
+// show dialog to get user input
+function getUserInput(numOfLayers){
+
+  // create new dialog
+  var alert = COSAlertWindow.new()
+
+  // dialog title & description
+  alert.setMessageText("Appender")
+  // alert.setInformativeText("")
+
+  // create text field and its lable
+  alert.addTextLabelWithValue("Text to append:")
+  alert.addTextFieldWithValue("")
+
+  // set it as first responder
+  var textField = alert.viewAtIndex(1)
+  alert.alert().window().setInitialFirstResponder(textField)
+
+  // action buttons
+  alert.addButtonWithTitle('Append') // 1000
+  alert.addButtonWithTitle('Cancel') // 1001
+
+  // show dialog
+  var buttonPressed =  alert.runModal()
+
+  // get user input
+  var userInput = alert.viewAtIndex(1).stringValue()
+
+  // return user input
+  return [buttonPressed, userInput]
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Appender
+Allows you to append text to multiple text layers at ones. Like a boss.
+
+## Preview
+
+## Usage
+
+## Installation
+1. Download [the latest ZIP file][1] of this plugin.
+2. Extract it.
+3. Double-click Appender.sketchplugin
+4. Have fun!
+
+## Future development
+This is a side project I wrote in my free time. If you got some free time you can contribute with the following features:
+* Append text layer to another.
+* Append multiple text layers to multiple text layers xD
+* or any other cool features.
+
+## Feedback
+If you discover any issues, please [open an issue][2].
+And ping me on [twitter][3] for questions and [follow me][3] for updates.
+
+## License
+This software is released under Apache License, see LICENSE.
+
+[1]: https://github.com/ahmedshaalan/appender-sketch-plugin/archive/master.zip
+[2]: https://github.com/ahmedshaalan/appender-sketch-plugin/issues
+[3]: https://twitter.com/ahmed_shaalan
